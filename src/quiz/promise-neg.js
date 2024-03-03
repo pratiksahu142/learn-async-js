1a4253403df5ac7a7867a2f4b750c8c6f7f94804
pratiksahu <pratik.sahu1421996@gmail.com>
2024-03-02 19:41:18 -0500

    Class Activity Feb 27

--- src/quiz/promise-neg.js
+++ src/quiz/promise-neg.js
@@ -3,3 +3,36 @@ const array2D = [
     [4, 5, 6],
     [7, 8, -9]
 ];
+
+function logWhenNegativeNumber(array, rowIndex) {
+    return new Promise((resolve, reject) => {
+        console.log('Log called ... ');
+        if (array.length > rowIndex) {
+            setTimeout(() => {
+                const hasNegative = array[rowIndex].some((e) => e < 0);
+                if (hasNegative) {
+                    console.log(`Row ${rowIndex} has at least one negative number.`);
+                    resolve(rowIndex);
+                } else {
+                    reject(`Row ${rowIndex} does not have any negative number.`);
+                }
+            }, 0);
+        } else {
+            console.log('Rejecting ... ');
+            reject('Index out of bounds');
+        }
+        console.log('Returning from log');
+    });
+}
+
+const promises = [];
+
+for (let x = 0; x < array2D.length; x++) {
+    promises.push(logWhenNegativeNumber(array2D, x));
+}
+
+Promise.any(promises)
+    .then((negativeRowIndex) => {
+        console.log(`Negative number found at row number: ${negativeRowIndex}`);
+    })
+    .catch((error) => console.log(error));
--- src/quiz/promise-sum-2darray.js
+++ src/quiz/promise-sum-2darray.js
@@ -3,3 +3,42 @@ const array2D = [
     [4, 5, 6],
     [7, 8, 9]
 ];
+
+
+function RowSum(arr,rowIndex){
+    return new Promise((resolve, reject) => {
+        console.log('Sum called ... ');
+        if(arr.length>rowIndex) {
+            setTimeout(() => {
+                let sum = 0;
+                for (let i = 0; i < arr[rowIndex].length; i++) {
+                        sum += arr[rowIndex][i];
+                    }
+
+                resolve(sum);
+            }, 0);
+        }
+        else {
+            console.log('rejecting ... ');
+            reject('index out of bounds');
+        }
+        console.log('returning from sum');
+
+})
+}
+
+promises=[];
+
+for(let x=0;x<array2D.length;x++){
+    promises.push(RowSum(array2D,x))
+}
+
+Promise.all(promises)
+    .then((rsum)=>{
+        let sum=0;
+        rsum.forEach(rsum =>{
+            sum+=rsum;
+        })
+        console.log(sum);
+    })
+    .catch((error)=> console.log(error))
